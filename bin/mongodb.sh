52abbb4c4681c27198502b19ac7ba7c314c0fb06
opera443399 <pc@pcswo.com>
2018-08-20 17:24:17 +0800

    fix(swarmlog): mongo volume path typo

--- bin/mongodb.sh
+++ bin/mongodb.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
-# 2018/7/25
+# 2018/8/20
 
 test $(docker ps -a -f name=logs-mongodb -q |wc -l) -eq 0 || \
 docker rm -f $(docker ps -a -f name=logs-mongodb -q)
 
-# ports: 9200[mongodb]
+# ports: 27017[mongodb]
 docker run -d -p "27017:27017" \
     --name logs-mongodb \
     -v /etc/localtime:/etc/localtime \
     -v /etc/timezone:/etc/timezone \
-    -v /data/server/swarmlog/mongodb:/usr/share/elasticsearch/data \
+    -v /data/server/swarmlog/mongodb:/data/db \
     mongo:3
