ee04b509d0321ab159d2da058ead806db33e1e10
opera443399 <pc@pcswo.com>
2018-12-17 12:31:36 +0800

    fix(agent): limit the resource and persist the filebeat registry to disk
    * do not redo the I/O process after the agent restart
    * limit the resource, as I found that in some cases it will take more than 1GB memory.

--- filebeat/bin/agent.sh
+++ filebeat/bin/agent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# 2018/11/15
+# 2018/12/17
 
 ELASTICSEARCH_HOST='10.50.200.101'
 ELASTICSEARCH_PORT='9200'
@@ -9,13 +9,17 @@ ELASTICSEARCH_PASSWORD='admin'
 
 test $(docker ps -a -f name=logs-agent -q |wc -l) -eq 0 || \
 docker rm -f logs-agent
+docker volume inspect logs-agent-var >/dev/null 2>&1 || docker volume create logs-agent-var
 
-docker run -d --rm -it \
+docker run -d --restart always \
     --name logs-agent \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v /etc/localtime:/etc/localtime \
+    -v logs-agent-var:/var/lib/filebeat \
     -v /:/host \
     --cap-add SYS_ADMIN \
+    --cpus "0.5" \
+    --memory "256m" \
     -e LOGGING_OUTPUT=elasticsearch \
     -e ELASTICSEARCH_HOST=${ELASTICSEARCH_HOST} \
     -e ELASTICSEARCH_PORT=${ELASTICSEARCH_PORT} \
